63f60e812fd91e83fa16e2b5cf1e4a3627921cb4
Sana <sanamukhtar969@gmail.com>
2024-03-24 03:03:24 +0530

    .

---
+++ src/redux/api.ts
@@ -0,0 +1,11 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+export const post = createApi({
+    reducerPath:"api",
+    baseQuery:fetchBaseQuery({
+        baseUrl:"http://localhost:3000",
+    }),
+    endpoints:()=>({})
+})
+
